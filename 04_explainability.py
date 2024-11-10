c87e02c2a63d23c5cc74451c76b9d45b9374e379
Ana Matoso <78906907+anamatoso@users.noreply.github.com>
2024-11-10 19:02:42 +0000

    Update

--- 04_explainability.py
+++ 04_explainability.py
@@ -1,198 +1,205 @@
+#%% 
+# Imports
 print("Importing...")
-import argparse
 import os
 import pickle
+import resource
 import sys
 
 import matplotlib.pyplot as plt
-import monai
 import numpy as np
 import pandas as pd
-from captum.attr import DeepLift, Saliency
-from captum.attr import visualization as viz
+import torch
+import torch.nn.functional as F
+
+from captum.attr import Saliency
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+
+import monai
+from monai.data import DataLoader, Dataset
+from monai.networks.nets import (Classifier, DenseNet121, DenseNet169,
+                                 DenseNet264, ResNet, ViT)
 from monai.utils import set_determinism
-from sklearn.metrics import confusion_matrix
 
-from models import *
-from utils import (convert2binary, get_data_and_transforms, get_loaders,
-                   get_model_setup, test)
-
-# CAPTUM
-# import torchvision
-# from captum.attr import IntegratedGradients
-# from captum.attr import NoiseTunnel
-
-CUDA = torch.cuda.is_available()
-DATASETS = "./Datasets"
-def parse_argument():
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--log_folder', type = str, required = True,
-                        help = 'the name of the log folder to test (basename)')
-    parser.add_argument('--n_folds', type = int, default = 5,
-                        help = 'an integer for number of folds, defaults to 5')
-    parser.add_argument('--weight_decay', type = int, default = 0.01,
-                        help = 'an integer for weight_decay, defaults to 0.01')
-    args = parser.parse_args()
-    return args
-
-if __name__ == '__main__':
-
-    arguments = parse_argument()
-    print("Testing on "+arguments.log_folder)
-    logs_folder    = "Pytorch_Logs"
-    if os.path.exists(os.path.join(logs_folder,arguments.log_folder,"confusion_matrix.csv")):
-        print(arguments.log_folder + " already done")
-        sys.exit(1)
-    
-    # Get results from that attempt
-    df = pd.read_csv('results.csv')
-    row = df[df['logdir'] == arguments.log_folder]
-    results = row.to_dict(orient='records')[0]
-
-    # Set main variables
-    phd_dir         = os.getcwd()
-    dataset         = results["dataset"]
-    classes         = ["PD", "SD", "PR", "CR"]
-    num_classes     = len(classes)
-    model_name      = results["model"]
-    subtract        = results["subtract"] == True
-    convert_bin     = results["binary"] == "True"
-    bs              = int(results["bs"])
-    learning_rate   = float(results["LR"])
-    sampler_weight  = results["sampler_weight"]
-    loss_weight     = results["loss_weight"]
-    weight_decay    = 0.01
-    seed            = 2
-    fold            = int(results["fold"])
-    set_determinism(seed)
-
-    # Get table with data info
-    print("Getting data...")
-    with open(os.path.join(phd_dir, "lumiere/table_all.pkl"), "rb") as f:
-        all = pickle.load(f)
-
-    # Create the data, get transforms and number of channels
-    data,  transforms_train, transforms_test, num_channels, labels = get_data_and_transforms(DATASETS, all, classes, subtract)
-
-    if convert_bin:
-        classes, num_classes, dataset = convert2binary(data, labels) 
-
-    # Divide data into train and test set and create each dataloader
-    print("Creating train and validation datasets...")
-    folds = monai.data.utils.partition_dataset_classes(data, labels, num_partitions = arguments.n_folds, seed = seed)
-
-    print("Getting loaders...")
-    class_prevalence, _, test_loader = get_loaders(classes, bs, sampler_weight, transforms_train, transforms_test, fold, folds)
-
-    device = torch.device("cuda" if CUDA else "cpu")
-    print("Getting model setup...")
-    model_config, model, _, _, _ = get_model_setup(model_name, class_prevalence, device, learning_rate, weight_decay, loss_weight, num_channels, num_classes)        
-
-    # Test model with unseen data
-    print("----------")
-    print("Testing model with unseen data...")
-    log_dir=os.path.join(logs_folder, arguments.log_folder)
-
-    if not os.path.exists(os.path.join(logs_folder,arguments.log_folder,"confusion_matrix.csv")):
-        result, real, predicted = test(model_config, log_dir, dataset, device, bs, classes, test_loader, model_name)
-        
-        # Calculate confusion matrix
-        labels=list(range(len(classes)))
-        confusion_mat = confusion_matrix(real,predicted,labels=labels)
-
-        np.savetxt(os.path.join(logs_folder,arguments.log_folder,"confusion_matrix.csv"), confusion_mat, delimiter=",")
-    
-    # Check if confusion matrix has been calculated else save it
-    if os.path.exists(os.path.join(logs_folder,arguments.log_folder,"confusion_matrix.png")):
-        print("Confusion matrix already done")
-        sys.exit(1)
-    else:
-        # Plot confusion matrix
-        plt.figure(figsize=(8, 6))
-        plt.imshow(confusion_mat,cmap="jet")
-        tick_marks = np.arange(len(classes))
-        plt.xticks(tick_marks, classes, rotation=45)
-        plt.yticks(tick_marks, classes)
-        for i in range(confusion_mat.shape[0]):
-            for j in range(confusion_mat.shape[1]):
-                plt.text(j, i, format(confusion_mat[i, j]),
-                        ha="center", va="center",size=20,
-                        color="white" if confusion_mat[i, j] > confusion_mat.max() / 2 else "black")
-
-        plt.xlabel('Predicted labels')
-        plt.ylabel('True labels')
-        plt.title('Confusion Matrix')
-        plt.colorbar(label="#samples")
-        plt.show()
-        plt.savefig(os.path.join(logs_folder,arguments.log_folder,"confusion_matrix.png"))
+from aux import get_data_and_transforms, plot_saliency_grid
+from models import (AlexNet3D, DenseNetWithClinical)
 
 
+cuda=torch.cuda.is_available()
 
-    dataiter = next(iter(test_loader))
-    images, labels = dataiter["images"], dataiter["label"]
-    
-    # print images
-    # plot_image(torchvision.utils.make_grid(images))
-    # print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+torch.cuda.empty_cache()
+rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
+resource.setrlimit(resource.RLIMIT_NOFILE, (2048, rlimit[1]))
 
-    outputs = model(images)
+#%% Variables that can change
+phd_dir        = os.getcwd()
+classes        = ["PD", "SD", "PR", "CR"]
+num_classes    = len(classes)
+logs_folder    = "./Pytorch_Logs"
+bs             = 1
+seed           = 2
+set_determinism(seed)
 
-    _, predicted = torch.max(outputs, 1)
+# CHANGE FOR BEST EXPERIMENT
+experiment     = "24-10-18_14.29.31"
+fold           = 1
 
-    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
-                                for j in range(4)))
-    
-    ind = 3
-    
+df = pd.read_csv('results.csv', header=None)
+row = df[df[0] == "Jul09_15-31-18_pluto"]
+experiment_list = row.iloc[0]
 
-    input_image = images[ind].unsqueeze(0)
-    labels_index = labels[ind]
-    input_image.requires_grad = True
-    model.eval()
+dataset        = "rano" + experiment_list[9]
+data_dir       = os.path.join("LUMIERE/Datasets/", dataset)
+subtract       = experiment_list[11]==True
+clinical_data  = experiment_list[-1]==True
+model_name     = experiment_list[15] # densenet264clinical monai_vit monai_resnet monai_classifier monai_densenet121 monai_densenet169 monai_densenet264 monai_densenet201 jang_model_mri AlexNet3D GoogleNet3D
 
-    def attribute_image_features(algorithm, image, labels_index, **kwargs):
-        model.zero_grad()
-        tensor_attributions = algorithm.attribute(torch.tensor(image),target=torch.tensor(torch.argmax(labels_index)),**kwargs)
-        return tensor_attributions
-    
-    saliency = Saliency(model)
-    grads = saliency.attribute(input_image, target=torch.argmax(labels_index))
-    
+log_dir        = os.path.join(logs_folder,experiment)
 
-    # ig = IntegratedGradients(model) # Verified until here
-    # attr_ig, delta = attribute_image_features(ig, input_image, labels_index, baselines=input_image * 0, return_convergence_delta=True)
-    # attr_ig = np.transpose(attr_ig.squeeze().cpu().detach().numpy(), (1, 2, 0))
-    # print('Approximation delta: ', abs(delta))
+#%% Create dataset to classify image types 
 
-    # ig = IntegratedGradients(model)
-    # nt = NoiseTunnel(ig)
-    # attr_ig_nt = attribute_image_features(nt, input_image, baselines=input_image * 0, nt_type='smoothgrad_sq',
-    #                                     nt_samples=100, stdevs=0.2)
-    # attr_ig_nt = np.transpose(attr_ig_nt.squeeze(0).cpu().detach().numpy(), (1, 2, 0))
-    
-    dl = DeepLift(model)
-    attr_dl = attribute_image_features(dl, input_image, labels_index, baselines=input_image * 0)
+print("Getting data...")
 
-    # unnormalized_image=input_image.squeeze()[:,:,:,slice_idx].squeeze()[0,:,:].unsqueeze(0).cpu().detach().numpy()
-    # maximum=np.max(np.max(unnormalized_image[0]))
-    # minimum=np.min(np.min(unnormalized_image[0]))
-    # unnormalized_image[0]=(unnormalized_image[0] - minimum) / (maximum - minimum)
+# Get table with data info
+with open(os.path.join(phd_dir,"table_classifyable.pkl"), "rb") as f:
+    all = pickle.load(f)
 
-    min_vals = input_image.amin(dim=(2, 3, 4), keepdim=True)
-    max_vals = input_image.amax(dim=(2, 3, 4), keepdim=True)
-    normalized_image = (input_image - min_vals) / (max_vals - min_vals)
-    
-    slice_idx=100
-    image_number=1
-    original_image=np.transpose(normalized_image[0,image_number,:,:,slice_idx].unsqueeze(0).cpu().detach().numpy(), (1, 2, 0))
-    grads_plot = np.transpose(grads.squeeze()[:,:,:,slice_idx].squeeze()[0,:,:].unsqueeze(0).cpu().detach().numpy(), (1, 2, 0))
-    attr_dl_plot = np.transpose(attr_dl.squeeze()[:,:,:,slice_idx].squeeze()[0,:,:].unsqueeze(0).cpu().detach().numpy(), (1, 2, 0))
-    _ = viz.visualize_image_attr(None, original_image, method="original_image", title="Original Image", show_colorbar=True)
+# Create the data, get transforms and number of channels
+data,  _, transforms_test, num_channels, labels = get_data_and_transforms(data_dir, all, classes, subtract, clinical_data)
 
-    _ = viz.visualize_image_attr(grads_plot, original_image, method="blended_heat_map", sign="absolute_value",show_colorbar=True, title="Overlayed Gradient Magnitudes") # Plots until here work
+#%% Divide data into train and test set and create dataloader
 
-    # _ = viz.visualize_image_attr(attr_ig, original_image, method="blended_heat_map",sign="all",show_colorbar=True, title="Overlayed Integrated Gradients")
+print("Creating train and validation datasets...")
 
-    # _ = viz.visualize_image_attr(attr_ig_nt, original_image, method="blended_heat_map", sign="absolute_value", outlier_perc=10, show_colorbar=True, title="Overlayed Integrated Gradients \n with SmoothGrad Squared")
+# Recreate fold from benchmark
+folds = monai.data.utils.partition_dataset_classes(data, labels, num_partitions = 5, seed = seed)
+test_data = folds[fold]
 
-    _ = viz.visualize_image_attr(attr_dl_plot, original_image, method="blended_heat_map",sign="all",show_colorbar=True, title="Overlayed DeepLift")
+# Create datasets
+test_ds  = Dataset(data = test_data, transform = transforms_test)
+test_loader  = DataLoader(test_ds, batch_size = bs, shuffle = True)
+
+
+# %%
+# Create DL model
+device = "cpu"
+
+#################
+# Model Options #
+#################
+if model_name == "monai_classifier":
+    model_config  = Classifier(in_shape=(num_channels,240,240,155), classes=num_classes, channels=(2,4,8), strides=(2,4,8),last_act="softmax")
+elif model_name == "monai_densenet121":
+    model_config  = DenseNet121(spatial_dims=3, in_channels=num_channels, out_channels=num_classes, pretrained=False)
+elif model_name == "monai_densenet169":
+    model_config  = DenseNet169(spatial_dims=3, in_channels=num_channels, out_channels=num_classes, pretrained=False)
+elif model_name == "monai_densenet264":
+    model_config  = DenseNet264(spatial_dims=3, in_channels=num_channels, out_channels=num_classes, pretrained=False)
+elif model_name == "monai_vit":
+    model_config  = ViT(in_channels=num_channels, img_size=[240,240,155], patch_size=[20,20,10], classification=True, num_classes=num_classes, pos_embed_type='sincos', dropout_rate=0.1)
+elif model_name == "monai_resnet":
+    model_config  = ResNet("Bottleneck", (3, 4, 6, 3), (64, 128, 256, 512), spatial_dims=3, n_input_channels=num_channels, num_classes=num_classes)
+elif model_name == "AlexNet3D":
+    model_config  = AlexNet3D(num_channels, num_classes = num_classes)  
+elif model_name == "medicalnet_resnet18":
+    from modelresnet import resnet18
+    model_config  = resnet18(sample_input_W=240, sample_input_H=240, sample_input_D=155, shortcut_type='A', no_cuda=False, num_seg_classes=4)  
+elif model_name == "densenet264clinical":
+    image_model  = DenseNet264(spatial_dims=3, in_channels=num_channels, out_channels=num_classes, pretrained=False)
+    model_config = DenseNetWithClinical(densenet_model=image_model, num_classes=num_classes, clinical_data_dim=5)
+else: sys.exit('Please choose one of the models available. You didnt write any one of them')
+print("Model imported")
+
+# Load the pre-trained model
+model = model_config.to(device)
+weights_path = os.path.join(log_dir,dataset + ".pt")
+model.load_state_dict(torch.load(weights_path, map_location = device),strict=False)
+model.eval()
+print("Weights loaded")
+
+it = iter(test_loader) # Create an input tensor image for your model.
+input_item = next(it)
+input_tensor = input_item["images"]
+print("batch loaded")
+print(input_item["label"])
+#%% Select class (do not run for first class and then from this section on for the other classes)
+
+while not torch.equal(input_item["label"],torch.tensor([[0,1,0,0]])): # change tensor for the classes 2 and 3
+    input_item = next(it)
+    input_tensor = input_item["images"]
+    print("batch loaded")
+    print(input_item["label"])
+# %% GRADCAM
+
+outputs=model(input_tensor.to(device))
+class_pred = int(torch.max(outputs))
+class_real = int(torch.argmax(input_item["label"]))
+
+target_real = [ClassifierOutputTarget(class_real)]
+target_pred = [ClassifierOutputTarget(class_pred)]
+
+target_layers = [model.features[10]] 
+
+torch.cuda.empty_cache()
+# Construct the CAM object
+with GradCAM(model=model, target_layers=target_layers) as cam:
+    grayscale_cam_real = cam(input_tensor=input_tensor, targets=target_real)
+    grayscale_cam_pred = cam(input_tensor=input_tensor, targets=target_pred)
+
+grayscale_cam_real = grayscale_cam_real[0, :]
+grayscale_cam_pred = grayscale_cam_pred[0, :]
+
+plot_saliency_grid(input_tensor[0,-3,:,:,:],grayscale_cam_real,
+                    overlay=True, cmap="jet", filename="images/final/gradcam_c"+str(class_real)+".svg")
+plot_saliency_grid(input_tensor[0,-3,:,:,:],grayscale_cam_pred, 
+                    overlay=True, cmap="jet", filename="images/final/gradcam_c"+str(class_real)+"_pred.svg")
+
+#plot_saliency_grid(input_tensor[0,-3,:,:,:],grayscale_cam, overlay=True, cmap="jet", filename=None)
+
+#make_gif_saliency(input_tensor[0,-3,:,:,:],grayscale_cam,"GradCAM-Layer_"+str(9)+"-target0")
+
+# %% Captum
+
+_, predicted = torch.max(outputs, 1)
+input_tensor.requires_grad=True
+print('Ground truth:', classes[input_item["label"].argmax()], "\n",
+    'Predicted:', classes[predicted], "\n",
+    'Probability:', torch.max(F.softmax(outputs, 1)).item(),"\n",
+    'Probability for all classes:', np.round(F.softmax(outputs, 1).tolist()[0],3)
+    )
+
+model.to("cpu")
+images=input_tensor.to("cpu").squeeze()
+channel=-3
+original_image =images.cpu().detach().numpy()
+original_image =np.flip(np.transpose(original_image, (0, 2, 1, 3)),1)
+
+saliency = Saliency(model)
+
+grads_pred = saliency.attribute(input_tensor, target=class_pred)
+grads_real = saliency.attribute(input_tensor, target=class_real)
+
+slice_list=[[55,77,99], [55,77,99], [66,77,88], [44,66,77]]
+
+for SLICE in slice_list[class_real]:
+
+    # Plot saliency maps for real class
+    max_image=np.max(np.max(np.max(grads_real)))
+    min_image=np.min(np.min(np.min(grads_real))) if np.min(np.min(np.min(grads_real))) !=0 else 0.0000001
+    plt.figure(figsize = (6, 6))
+    plt.imshow(grads_real[0,channel,:, :, SLICE].T, cmap = 'gray', origin = 'lower',vmin=min_image,vmax=max_image, norm="log")
+    plt.title(f'Axial Slice at z={SLICE}')
+    plt.colorbar()
+    plt.savefig("images/final/saliency_c"+str(class_real)+"_s"+str(SLICE)+".svg", format="svg", dpi=500)
+    plt.show()
+
+    # Plot saliency maps for predicted class
+    max_image=np.max(np.max(np.max(grads_pred)))
+    min_image=np.min(np.min(np.min(grads_pred))) if np.min(np.min(np.min(grads_pred))) !=0 else 0.0000001
+    plt.figure(figsize = (6, 6))
+    plt.imshow(grads_pred[0,channel,:, :, SLICE].T, cmap = 'gray', origin = 'lower',vmin=min_image,vmax=max_image, norm="log")
+    plt.title(f'Axial Slice at z={SLICE}')
+    plt.colorbar()
+    plt.savefig("images/final/saliency_c"+str(class_real)+"_s"+str(SLICE)+"_pred.svg", format="svg", dpi=500)
+    plt.show()
